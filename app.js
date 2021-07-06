b5c08a77324563f34c786ace9e89112836336ca1
Hanady khrishah <hanady.awad94@yahoo.com>
2021-07-06 10:49:54 +0300

    final exam

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let myForm = document.getElementById('myForm');
+let table = document.getElementById('Table');
+
+let list = [];
+
+function movieList (name , image ,release){
+
+
+    
+}
+
